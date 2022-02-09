1f941db6ec93662dc2146c2ae3ea7a899fc39a6f
narimenmokrani1 <narimenmokrani@gmail.com>
2022-02-09 07:56:27 -0600

    scaffold my aboutme.js

---
+++ src/components/aboutMe/AboutMe.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function AboutMe(props) {
+	return (
+		<div>
+			<h2>About Narimen Mokrani</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero deleniti
+				voluptatum rerum, cupiditate, perspiciatis magni tempore numquam natus
+				facere quibusdam veniam exercitationem dolorem, at modi quaerat nesciunt
+				fuga temporibus magnam.
+			</p>
+            <h3>Skills</h3>
+            <div className='skills'>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul><li></li>
+            <li></li>
+            <li></li>
+            <li></li></ul>
+            </div>
+		</div>
+	);
+}
+
+export default AboutMe;
