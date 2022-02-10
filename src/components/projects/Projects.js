cbb324af145608fc3a033ca9043258a8e4c063eb
narimenmokrani1 <narimenmokrani@gmail.com>
2022-02-10 07:44:12 -0600

    scaffold project.js

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Projects(props) {
+	return (
+		<div>
+			<div>
+				<img src='' alt='' />
+                <h2>Planet Memory Game</h2>
+			</div>
+			<div>
+				<img src='' alt='' />
+                <h2>Make-up Finder App</h2>
+			</div>
+			<div>
+				<img src='' alt='' />
+                <h2>Plant Haven</h2>
+			</div>
+			<div>
+				<img src='' alt='' />
+                <h2>Project 4git </h2>
+			</div>
+		</div>
+	);
+}
+
+export default Projects;
